792733ec4aa2a1bdc0d6849c55dfc7cda822ac7e
puglifebra <ghobart2024@chadwickschool.org>
2022-01-13 11:45:20 -0800

    chore: Initialize project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ghobart2024_DataEncodingInCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
