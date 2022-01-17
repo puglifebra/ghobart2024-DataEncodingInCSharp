61b96df6029b2d36e79142c187270c76b94c3240
puglifebra <ghobart2024@chadwickschool.org>
2022-01-17 11:30:22 -0800

    Im Confused

--- Program.cs
+++ Program.cs
@@ -6,16 +6,16 @@ namespace ghobart2024_DataEncodingInCSharp
     {
         static void Main(string[] args)
         {
-            Cipher cipher;
-            cipher = new Cipher(5);
+            string message;
+            message = System.IO.File.ReadAllText("secrets/1.txt");
+            Console.WriteLine($"The encrypted message is: '{message}'.");
 
-            string encrypted;
-            encrypted = cipher.Encrypt("rosebud");
-            Console.WriteLine($"The encrypted message is: '{encrypted}'");
-
-            string decrypted;
-            decrypted = cipher.Decrypt(encrypted);
-            Console.WriteLine($"The decriptyed message is: '{decrypted}'");
+            string decryptedMessage = string.Empty;
+            foreach (char c in message)
+            {
+                message += (char)(c - 5);
+            }
+            return message;
         }
     }
 }
