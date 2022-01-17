1f24d13dc738320892846ca15362c3d02cf0ede1
puglifebra <ghobart2024@chadwickschool.org>
2022-01-17 11:16:58 -0800

    feat: Add Decrypt method

--- Program.cs
+++ Program.cs
@@ -12,6 +12,10 @@ namespace ghobart2024_DataEncodingInCSharp
             string encrypted;
             encrypted = cipher.Encrypt("rosebud");
             Console.WriteLine($"The encrypted message is: '{encrypted}'");
+
+            string decrypted;
+            decrypted = cipher.Decrypt(encrypted);
+            Console.WriteLine($"The decriptyed message is: '{decrypted}'");
         }
     }
 }
